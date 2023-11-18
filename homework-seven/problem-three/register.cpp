4e14f4e26d82dee1c210f2bad2b00c137c320f94
Anahit <anah.arakelyan@gmail.com>
2023-11-18 15:17:57 +0000

    added Makefiles

--- homework-seven/problem-three/register.cpp
+++ homework-seven/problem-three/register.cpp
@@ -7,21 +7,27 @@
 #include <inttypes.h>
 
 
-void sigusr1_handler(int signum, siginfo_t* info, void* context)
+void sigusr1_handler(int signum, siginfo_t* info, void* vcontext)
 {
 	std::cout<<"PokemonID "<<getpid()<<std::endl;
-	uint32_t ceip = 0, ceax = 0, cebx = 0 ;
+	//uint32_t ceip = 0, ceax = 0, cebx = 0 ;
 	//asm volatile ("mov %%eip, %[Var]" : [Var] "=r"(ceip));
-	asm volatile ("mov %%eax, %[Var]" : [Var] "=r"(ceax));
-	asm volatile ("mov %%ebx, %[Var]" : [Var] "=r"(cebx));		
-	
+	//asm volatile ("mov %%eax, %[Var]" : [Var] "=r"(ceax));
+	//asm volatile ("mov %%ebx, %[Var]" : [Var] "=r"(cebx));		
+	//uint64_t rax = 0, rbx = 0;
+	//__asm__("" : "=a"(rax), "=b"(rbx) ::);
 	//register int eax asm("eax");
 	//register int eax asm("ebx");
 	//int ceax = eax;
 	//int cebx = ebx;
 
+	auto *context = static_cast<ucontext_t *>(vcontext);
+	auto mcontext = context->uc_mcontext;
+        std::cout << "-- RIP register : " << mcontext.gregs[REG_RIP] << std::endl;
+        std::cout << "-- RAX register : " << mcontext.gregs[REG_RAX] << std::endl;
+        std::cout << "-- RBX register : " << mcontext.gregs[REG_RBX] << std::endl;
 		
-	std::cout << "Stats:\n EIP: " << ceip << "\nEAX: " << ceax << "\nEBX: " << cebx <<std::endl;
+	//std::cout << "Stats:\n EIP: " << 0 << "\nEAX: " << rax << "\nEBX: " << rbx <<std::endl;
 }
 
 
