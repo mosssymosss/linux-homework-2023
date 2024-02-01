9c991e0a89a7c9eb204aa5e51d0ea353784237d4
Anahit Arakelyan <123606322+mosssymosss@users.noreply.github.com>
2024-02-01 16:05:23 +0400

    Revert "Third homework"

---
+++ homework-two/mycat.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cerrno>
+#include <vector>
+#include <fcntl.h>
+#include <unistd.h>
+
+const std::size_t buffer_size = 4096;
+
+int main(int argc, char** argv)
+{
+    if(argc != 2)
+    {
+        std::cerr  << errno << ": wrong arguments";
+        exit(EXIT_FAILURE);
+    }
+    const char* file = argv[1];
+    int file_id = open(file, O_RDONLY);
+    if(file_id == -1)
+    {
+        std::cerr  << errno << ": cannot open file";
+        exit(EXIT_FAILURE);
+    }
+    std::vector<char> buffer(buffer_size, '\0');
+    ssize_t bytes_read;
+    while(0 != (bytes_read = read(file_id, buffer.data(), buffer_size)))
+    {
+	if(bytes_read < 0)
+	{
+	    std::cerr << "read failed: ";	
+            exit(EXIT_FAILURE);
+	}
+        write(1, buffer.data(), bytes_read);
+	
+    }
+
+    close(file_id);
+    return 0;
+}
+
